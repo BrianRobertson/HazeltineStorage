25b39db136440e2eb4aefb693aba7d1419c33589
BrianRobertson <briaonrobertson@gmail.com>
2017-05-22 08:24:10 -0500

    Add initial project files.

---
+++ HazeltineStorage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HazeltineStorage.Startup))]
+namespace HazeltineStorage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
