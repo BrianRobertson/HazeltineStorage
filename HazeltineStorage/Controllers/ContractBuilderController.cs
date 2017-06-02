9b90d2823d331dfdf52feb1fd3d312a867346861
BrianRobertson <briaonrobertson@gmail.com>
2017-06-02 14:26:57 -0500

    ContractBuilderViewModel added with BuildContract view.

---
+++ HazeltineStorage/Controllers/ContractBuilderController.cs
@@ -0,0 +1,38 @@
+﻿using HazeltineStorage.Models;
+using HazeltineStorage.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HazeltineStorage.Controllers
+{
+    public class ContractBuilderController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: ContractBuilder
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET BuildContract
+        public ActionResult BuildContract()
+        {
+            var viewModel = new ContractBuilderViewModel();
+
+            return View(viewModel);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
